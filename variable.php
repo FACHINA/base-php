95eb22ac797d1835b0a818460abf2972f4e49b18
FACHINA <andersonfachina@outlook.fr>
2025-02-15 13:07:02 +0100

    Update

--- variable.php
+++ variable.php
@@ -2,15 +2,18 @@
 
 $nom = readline('Entrer votre nom : ');
 $prenom = readline('Entrer votre prénom : ');
+$age = readline('Entrer votre âge : ');
 
 // Avec les doubles quotes
-// echo "Bonjour $prenom $nom";
+// echo "Bonjour $prenom $nom \n";
+// echo "L'age de $prenom est $age ans \n";
 
 // Avec les simples quotes (ne fonctionne pas)
 // echo 'Bonjour $prenom $nom';
 
 // Avec les simples quotes et la concaténation
-// echo 'Bonjour ' . $prenom . ' ' . $nom;
+echo 'Bonjour ' . $prenom . ' ' . $nom . "\n";
+echo 'L\'age de ' . $prenom . ' est ' . $age . ' ans';
 
 // Avec les doubles quotes et la concaténation
 // echo "Bonjour " . $prenom . " " . $nom;
